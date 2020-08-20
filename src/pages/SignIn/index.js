87e08cbbc221a1d3d5bfaaf55bf61733082c779b
gabriellmandelli <gabriel-mandelli@hotmail.com>
2020-08-19 22:39:58 -0300

    first commit

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,45 @@
+import * as Yup from 'yup';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
+
+import { signInRequest } from '../../store/modules/auth/actions';
+
+// import { Container } from './styles';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
+
+function SignIn() {
+  const dispatch = useDispatch();
+  const loading = useSelector(state => state.auth.loading);
+
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
+  }
+
+  return (
+    <>
+      <img src="https://www.softplan.com.br/wp-content/themes/softplan-2019/imagens/logo-softplan.svg" alt="SoftPlayer" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
+
+        <button type="submit">{loading ? 'Carregando...' : 'Acessar'}</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </Form>
+    </>
+  );
+}
+
+export default SignIn;
---
+++ src/pages/_layouts/default/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  height: 100%;
+  background: linear-gradient(-90deg, #00bfff, #3100CE);
+`;
+
+//00bfff
